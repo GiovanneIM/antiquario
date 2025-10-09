1bdc5cb80e6ccebec5fbc4e543bc7180723f1d72
GIOVANNE ISAAC MARQUES <25170154@educ123.sp.senai.br>
2025-10-09 10:04:52 -0300

    Header

--- cassadipieta/src/components/blocks/header/index.jsx
+++ cassadipieta/src/components/blocks/header/index.jsx
@@ -3,90 +3,92 @@ import Link from "next/link"
 
 export default function Header() {
     return (<>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-            <div className="container-fluid">
-                <div>
-                    <Link className="nav-link" href="/">
-                        <img src="/img/cassadPieta.svg" height={64}/>
-                    </Link>
-                </div>
+        <header>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+                <div className="container-fluid">
+                    <div>
+                        <Link className="nav-link" href="/">
+                            <img src="/img/cassadPieta.svg" height={64} />
+                        </Link>
+                    </div>
 
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNavDropdown"
-                    aria-controls="navbarNavDropdown"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon" />
-                </button>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavDropdown"
+                        aria-controls="navbarNavDropdown"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon" />
+                    </button>
 
-                <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link className="nav-link active" href="/">
-                                Home
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/mostruario">
-                                Mostruário
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" href="/contato">
-                                Contato
-                            </Link>
-                        </li>{" "}
-                        <li className="nav-item dropdown">
-                            <a
-                                className="nav-link dropdown-toggle"
-                                href="#"
-                                role="button"
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false"
-                            >
-                                Produtos
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Action
-                                    </a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Another action
-                                    </a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Something else here
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className="nav-link active" href="/">
+                                    Home
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/mostruario">
+                                    Mostruário
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" href="/contato">
+                                    Contato
+                                </Link>
+                            </li>{" "}
+                            <li className="nav-item dropdown">
+                                <a
+                                    className="nav-link dropdown-toggle"
+                                    href="#"
+                                    role="button"
+                                    data-bs-toggle="dropdown"
+                                    aria-expanded="false"
+                                >
+                                    Produtos
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li>
+                                        <a className="dropdown-item" href="#">
+                                            Action
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item" href="#">
+                                            Another action
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item" href="#">
+                                            Something else here
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
 
-                <div className='divLogin'>
-                    <ul className="listaLogin">
-                        <li className="nav-item">
-                            <a className="nav-link" aria-current="page" href="/login">
-                                Login
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/cadastro">
-                                Cadastro
-                            </a>
-                        </li>
-                    </ul>
+                    <div className='divLogin'>
+                        <ul className="listaLogin">
+                            <li className="nav-item">
+                                <a className="nav-link" aria-current="page" href="/login">
+                                    Login
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/cadastro">
+                                    Cadastro
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
-        <div className="divisoria"></div>
+            </nav>
+            <div className="divisoria"></div>
+        </header>
     </>);
 }
