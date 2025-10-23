9fbf31f57e5f32ebd88cb3a3d961c4ecb1e0c5b2
GIOVANNE ISAAC MARQUES <25170154@educ123.sp.senai.br>
2025-10-23 09:15:20 -0300

    linkCard

--- cassadipieta/src/app/mostruario/page.jsx
+++ cassadipieta/src/app/mostruario/page.jsx
@@ -14,9 +14,10 @@ export default function prod() {
         <div className="container my-5">
             <div className="row">
                 <div className="col-12 d-flex flex-wrap justify-content-center gap-3">
-                    {produtos.map((produto, index) => (
+                    {produtos.map((produto) => (
                         <Card
-                            key={index}
+                            key={produto.id}
+                            id={produto.id}
                             nome={produto.nome}
                             img={produto.img}
                             preco={produto.preco}
